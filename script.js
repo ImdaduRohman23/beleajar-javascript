468084651714f28d801c925301f4d4e70dd71d5a
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-21 14:43:24 +0700

    belajaran higher order function

--- script.js
+++ script.js
@@ -15,3 +15,18 @@
 //     console.log(`halo ${nama}, selamat ${waktu}`)
 // }
 // sapa('imdadu', 'pagi')
+
+
+
+// ---------- HIGER ORDER FUNCTION ----------
+// => fungsi yg beroperasi pada fungsi lain, baik digunakan pada argument maupun sbg return value
+const sapa = (nama, waktu) => {
+    console.log(nama);
+    waktu();
+}
+
+const waktu = () => {
+    alert('selalamt malam')
+}
+
+sapa('imdadu', waktu)
