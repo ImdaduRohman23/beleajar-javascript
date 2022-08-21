e97e269a9c35fd41db13331646aa7d3a9fd411fd
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-21 14:24:55 +0700

    belajar arrow function

---
+++ script.js
@@ -0,0 +1,17 @@
+// ---------- ARROW FUNCTION ----------
+//declaration function
+// function sapa(nama) {
+//     console.log(`halo ${nama}`)
+// }
+
+// function expression
+// const sapa = function(nama) {
+//     console.log(`halo ${nama}`)
+// }
+// sapa('imdadu')
+
+// ARROW FUNCTION
+// const sapa = (nama, waktu) => {
+//     console.log(`halo ${nama}, selamat ${waktu}`)
+// }
+// sapa('imdadu', 'pagi')
