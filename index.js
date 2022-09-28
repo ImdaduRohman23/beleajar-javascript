9939ffabf453f9c82e7f95f31952fd62979a84ca
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-28 19:40:57 +0700

    scop variable

--- index.js
+++ index.js
@@ -16,9 +16,33 @@
 // console.log(string, number, isTrue, isFalse);
 
 // 4. UNDEFINED VS NULL
-var nama; //4. undifined
-console.log(nama)
-var nol = null; //5. null
-console.log(nol);
+// var nama; //4. undifined
+// console.log(nama)
+// var nol = null; //5. null
+// console.log(nol);
 
+//5. SCOP VARIABLE
+var global = 'global'; //global variable
 
+if(true) {
+    console.log('if', global);
+    var ifif = 'if'; //global variable => variabel di dalam block if
+}
+
+for(i = 0; i<1; i++) {
+    console.log('for', global);
+    var forfor = 'for'; //global variable => variabel di dalam block for
+
+}
+
+function test() {
+    console.log('function', global);
+    var fungsi = 'function'; //local variable => variabel di dalam fungsi
+}
+test();
+
+console.log(ifif)
+console.log(forfor)
+console.log(fungsi)
+
+//KESIMPULAN = HANYA VARIABEL DI DALAM FUNGSI YANG MENGHASILKAN LOCAL VARIABLE
