ec948b96b5ff625a2c622e1e2814ee166d302875
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-28 19:02:06 +0700

    penulisan variabel dasar

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+// Perhatian! Hapus komen untuk melihat hasil program
+
+// 1. PROGRAM PERTAMA
+// console.log('Hello World!'); 
+
+// 2. PENULISAN VARIABEL (DASAR)
+var nama;
+nama = 'imdadu';
+console.log(nama)
