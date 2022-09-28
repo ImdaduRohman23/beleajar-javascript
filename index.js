b5f13d965dd9a563993b2499d552669cd392d6b5
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-28 19:10:10 +0700

    data type

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 // console.log('Hello World!'); 
 
 // 2. PENULISAN VARIABEL (DASAR)
-var nama;
-nama = 'imdadu';
-console.log(nama)
+// var nama; //variable declaration
+// nama = 'imdadu'; //variable assigment
+// console.log(nama)
+
+// 3. DATA TYPE
+var string = 'this is string data'; //string
+var number = 1; //number
+var isTrue = true; //boolean
+var isFalse = false; 
+console.log(string, number, isTrue, isFalse);
