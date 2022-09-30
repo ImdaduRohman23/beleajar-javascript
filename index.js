9dce6bc5160bf5c7315888c63ba70fd847f04d8a
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-30 18:22:17 +0700

    array => push

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 //ARRAY
 
-//---------- 1.Length ----------//
-let arr = [1, 2, 3, 4, 5];
-console.log(arr.length);
-console.log(arr[0]);
+//---------- 1.Length ----------// meretrun panjang array
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+// console.log(arr[0]);
 
-for(let i = 0; i < arr.length; i++) {
-    console.log('ke-', arr[i]);
+// for(let i = 0; i < arr.length; i++) {
+//     console.log('ke-', arr[i]);
+// }
+
+//---------- 1.Push ----------// menambahkan item di akhir array
+let arr = [1, 2, 3, 4, 5];
+arr.push(8);
+console.log(arr);
+for (let i = 1; i < arr.length; i++) {
+    console.log(arr[i])
 }
 
