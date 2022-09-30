6a0f4b77569428ac42c4b797db15a4de67d89178
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-30 15:19:45 +0700

    for

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Perhatian! Hapus komen untuk melihat hasil program
 
-//PENGULANGAN (alur flow 2)
+//PENGULANGAN/LOOPING (alur flow 2)
 
 // //---------- 1.Increment and Decrement ------------
 // //increment
@@ -17,3 +17,16 @@
 // j--;
 // j--;
 // console.log(j)
+
+// //---------- 1.For ------------
+// for(let i = 5; i <= 10; i++) {
+//     console.log(i)
+// };
+// for(let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+// console.log('end program for')
+
+let a = 1;
+++a;
+console.log(a)
