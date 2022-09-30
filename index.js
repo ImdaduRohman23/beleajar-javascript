01dde99522e49e3e03ea238fd896762141b3ca84
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-30 20:16:50 +0700

    array => unshift

--- index.js
+++ index.js
@@ -11,11 +11,17 @@
 //     console.log('ke-', arr[i]);
 // }
 
-//---------- 1.Push ----------// menambahkan item di akhir array
-let arr = [1, 2, 3, 4, 5];
-arr.push(8);
-console.log(arr);
-for (let i = 1; i < arr.length; i++) {
-    console.log(arr[i])
-}
+//---------- 2.Push ----------// menambahkan item di akhir array
+// let arr = [1, 2, 3, 4, 5];
+// arr.push(8);
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+//---------- 2.Unshift ----------// menambahkan item di awal array
+let arr = [1, 2, 3, 4];
+console.log('arr awal', arr)
+arr.unshift(10);
+console.log('arr unshift', arr)
 
