dcab5c6be08bb39dd63a482c942526bb15f90a7c
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-30 20:26:08 +0700

    sort

---
+++ 5ArayMethod.js
@@ -0,0 +1,27 @@
+// Perhatian! Hapus komen untuk melihat hasil program
+
+//ARRAY
+
+//---------- 1.Length ----------// meretrun panjang array
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+// console.log(arr[0]);
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log('ke-', arr[i]);
+// }
+
+//---------- 2.Push ----------// menambahkan item di akhir array
+// let arr = [1, 2, 3, 4, 5];
+// arr.push(8);
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+//---------- 2.Unshift ----------// menambahkan item di awal array
+// let arr = [1, 2, 3, 4];
+// console.log('arr awal', arr)
+// arr.unshift(10);
+// console.log('arr unshift', arr)
+
--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 // Perhatian! Hapus komen untuk melihat hasil program
 
-//ARRAY
+//ARRAY (Hight Order Function/Method)
+let arr = [9, 4, 8, 90, 6, 10, 2, 4, 1];
+arr.sort((a,b) => a -b);
+console.log(arr)
 
-//---------- 1.Length ----------// meretrun panjang array
-// let arr = [1, 2, 3, 4, 5];
-// console.log(arr.length);
-// console.log(arr[0]);
-
-// for(let i = 0; i < arr.length; i++) {
-//     console.log('ke-', arr[i]);
-// }
-
-//---------- 2.Push ----------// menambahkan item di akhir array
-// let arr = [1, 2, 3, 4, 5];
-// arr.push(8);
-// console.log(arr);
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i])
-// }
-
-//---------- 2.Unshift ----------// menambahkan item di awal array
-let arr = [1, 2, 3, 4];
-console.log('arr awal', arr)
-arr.unshift(10);
-console.log('arr unshift', arr)
 
