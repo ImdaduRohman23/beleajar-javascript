831a24cf72d4aad00fd405faad98f65ca0ace7aa
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-30 21:04:09 +0700

    filter

--- index.js
+++ index.js
@@ -10,10 +10,16 @@
 // console.log('b - a', arr)
 
 //--------- 2.Map ----------//
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-let arr2 = arr.map(item => item*2);
-console.log(arr2)
+// let arr2 = arr.map(item => item*2);
+// console.log(arr2);
+
+//--------- 2.Filter ----------//
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let arrFind = arr.filter((item, index) => (
+    console.log(item, index)
+));
 
 
 
