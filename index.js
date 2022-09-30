d8208dbef93fbc7316109e5bd895a8a85bbb010f
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-30 20:38:30 +0700

    map

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Perhatian! Hapus komen untuk melihat hasil program
 
 //ARRAY (Hight Order Function/Method)
-let arr = [9, 4, 8, 90, 6, 10, 2, 4, 1];
-arr.sort((a,b) => a -b);
-console.log(arr)
+
+//--------- 1.Sort ----------//
+// let arr = [9, 4, 8, 90, 6, 10, 2, 4, 1];
+// arr.sort((a,b) => a - b);
+// console.log('a -b', arr)
+// arr.sort((a,b) => b - a);
+// console.log('b - a', arr)
+
+//--------- 2.Map ----------//
+let arr = [1, 2, 3, 4, 5];
+
+let arr2 = arr.map(item => item*2);
+console.log(arr2)
+
 
 
