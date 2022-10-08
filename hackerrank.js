0caee01ce77e4470beead47e17264bfdaafada85
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-10-08 13:46:50 +0700

    hackerrank

---
+++ hackerrank.js
@@ -0,0 +1,44 @@
+// let arr = [-2, -5, 0, 2, 6];
+// let sumPlus = [];
+// for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] > 0) {
+//         sumPlus.push(arr[i]);
+//     }
+// }
+// console.log(sumPlus.length/arr.length);
+
+// function plusMinus(arr) {
+//     // Write your code here
+//     let plus = 0;
+//     let minus = 0;
+//     let zero = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] > 0) {
+//             plus++
+//         } else if (arr[i] < 0) {
+//             minus++
+//         } else if (arr[i] == 0) {
+//             zero++
+//         }
+//     }
+//     return [
+//         plus/arr.length,
+//         minus/arr.length,
+//         zero/arr.length
+//     ]
+// }
+
+// console.log(plusMinus([2, -1, 0, -1, -1]))
+
+// var a = '4';
+// var b = 4;
+// console.log(parseFloat(a))
+
+// function factorial(n) {
+//     let jmlh = 1;
+//     for( let i = n; i > 0; i--){
+//             jmlh *= i;
+//         }
+//     console.log(jmlh)
+// }
+// factorial(4);
