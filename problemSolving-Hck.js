d41e0a89b4bdef787b58638f4296fdb42aed0f74
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-10-18 17:49:12 +0700

    problem solving => Divisible Sum Pairs

--- problemSolving-Hck.js
+++ problemSolving-Hck.js
@@ -178,7 +178,6 @@ function breakingRecords(scores) {
         if(scores[i] > hight) {
             resH++
             hight = scores[i];
-
         }
         else if(scores[i] < low) {
             resL++
@@ -188,4 +187,18 @@ function breakingRecords(scores) {
     }
     return [resH, resL]
     
+}
+
+//Divisible Sum Pairs
+function divisibleSumPairs(n, k, ar) {
+    // Write your code here
+    let res = 0;
+    for(let i = 0; i < ar.length; i++){
+        for(let j = i+1; j < ar.length; j++){
+            if(ar[i] + ar[j] == k || (ar[i] + ar[j])%k == 0 && i<j){
+                res++
+            }
+        }
+    }
+    return res
 }
